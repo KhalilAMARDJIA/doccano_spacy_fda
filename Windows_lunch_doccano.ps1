b83450ae8f9c498edf87097e957f6be3f9a22a64
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2023-01-24 23:04:09 +0100

    working windows docc

--- Windows_lunch_doccano.ps1
+++ Windows_lunch_doccano.ps1
@@ -1,10 +1,10 @@
-# activate virtual environment
-& "$env:USERPROFILE\Documents\PROJECTS\.spent_venv\Scripts\activate.ps1"
+# Set path to virtual environment
+$VENV_PATH = "$env:USERPROFILE\Documents\PROJECT\.spent_venv\Scripts\activate.ps1"
 
-# navigate to the backend directory
-Set-Location "$env:USERPROFILE\Documents\PROJECTS\.spent_venv\lib\python3.10\site-packages\backend"
+# Set path to backend directory
+$BACKEND_PATH = "$env:USERPROFILE\Documents\PROJECT\.spent_venv\lib\site-packages\backend"
 
-# start the doccano webserver on an available port
+# select available port
 for ($port = 8000; $port -le 9000; $port++) {
     $tcp = New-Object System.Net.Sockets.TcpClient
     try {
@@ -13,14 +13,14 @@ for ($port = 8000; $port -le 9000; $port++) {
         break
     }
 }
-Write-Host "Starting server with port $port."
-Start-Process -FilePath "doccano" -ArgumentList "webserver --port $port" -NoNewWindow -Wait
 
-# start the doccano task in a separate PowerShell window
-Start-Process PowerShell -ArgumentList "-NoExit", "-Command", "& { & `"$env:USERPROFILE\Documents\PROJECTS\.spent_venv\Scripts\activate.ps1`"; Set-Location `"$env:USERPROFILE\Documents\PROJECTS\.spent_venv\lib\python3.10\site-packages\backend`"; & `"doccano task`"; }"
+# Open first terminal window and activate virtual environment, change directory and run doccano webserver with selected port
+Start-Process powershell.exe -ArgumentList "-NoExit", "-Command &{&'$VENV_PATH'; cd '$BACKEND_PATH'; doccano webserver --port $port;}"
 
-# wait for the webserver to start
-Start-Sleep -s 5
+
+# Open second terminal window and activate virtual environment, change directory and run doccano task
+Start-Process powershell.exe -ArgumentList "-NoExit", "-Command &{&'$VENV_PATH'; cd '$BACKEND_PATH'; doccano task;}"
+
 
 # open the website in the default browser
 Start-Process "http://localhost:$port/"
