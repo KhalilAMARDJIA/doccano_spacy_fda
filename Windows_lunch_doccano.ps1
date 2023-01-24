e4a5b3de5cf478e9bb13dbf922612737e8b48362
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2023-01-24 22:25:09 +0100

    windows doccano update

--- Windows_lunch_doccano.ps1
+++ Windows_lunch_doccano.ps1
@@ -1,10 +1,10 @@
-$VENV_PATH = "C:\Users\spent\Documents\PROJECTS\.spent_venv\Scripts\Activate.ps1"
-$BACKEND_PATH = "C:\Users\spent\Documents\PROJECTS\.spent_venv\Lib\site-packages\backend"
+# activate virtual environment
+& "$env:USERPROFILE\Documents\PROJECTS\.spent_venv\Scripts\activate.ps1"
 
-& $VENV_PATH
-cd $BACKEND_PATH
+# navigate to the backend directory
+Set-Location "$env:USERPROFILE\Documents\PROJECTS\.spent_venv\lib\python3.10\site-packages\backend"
 
-# Find an available port
+# start the doccano webserver on an available port
 for ($port = 8000; $port -le 9000; $port++) {
     $tcp = New-Object System.Net.Sockets.TcpClient
     try {
@@ -13,12 +13,11 @@ for ($port = 8000; $port -le 9000; $port++) {
         break
     }
 }
-
 Write-Host "Starting server with port $port."
-& doccano webserver --port $port
+Start-Process -FilePath "doccano" -ArgumentList "webserver --port $port" -NoNewWindow -Wait
 
-# open new terminal for doccano task
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command", "& $VENV_PATH; cd $BACKEND_PATH; doccano task"
+# start the doccano task in a separate PowerShell window
+Start-Process PowerShell -ArgumentList "-NoExit", "-Command", "& { & `"$env:USERPROFILE\Documents\PROJECTS\.spent_venv\Scripts\activate.ps1`"; Set-Location `"$env:USERPROFILE\Documents\PROJECTS\.spent_venv\lib\python3.10\site-packages\backend`"; & `"doccano task`"; }"
 
 # wait for the webserver to start
 Start-Sleep -s 5
